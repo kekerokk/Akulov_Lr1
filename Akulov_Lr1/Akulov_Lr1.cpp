bdd8f65cd48d2331bf6b62ff85a3df6ed4110257
Визуал студио ! <vspc_vs@VSPC34.RU>
2021-03-22 03:10:24 -0700

    Add project files.

---
+++ Akulov_Lr1/Akulov_Lr1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+void zadanie_1(int N,int p)
+{
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "ваыпы";
+}
---
+++ Akulov_Lr1/RosComNadzor.cpp
@@ -0,0 +1,16 @@
+#include "RosComNadzor.h"
+#include "locale.h"
+#include <iostream>
+
+using namespace std;
+
+void RosComNadzor::zadanie_1()
+{
+	setlocale(LC_ALL, "ru");
+	int N, p;
+	cout << "Введите десятичное число:\n";
+	cin >> N;
+	cout << "Введите систему счисления(цифру):\n";
+	cin >> p;
+
+}
