94b3171aebfb6baca3de656fa071f1669487c6b3
Иван <chelove4edd@mail.ru>
2021-03-23 16:13:19 +0300

    Izmenenie1

--- Akulov_Lr1/Akulov_Lr1.cpp
+++ Akulov_Lr1/Akulov_Lr1.cpp
@@ -3,13 +3,37 @@
 
 using namespace std;
 
-void zadanie_1(int N,int p)
+int zadanie_1(int N,int p)
 {
-
+	if (N == 1 || N == 0)
+	{
+		if (N == 1)
+			return N;
+		if (N == 0)
+			return N;
+	}
+	int* SS = new int[10];
+	int END = 0;
+	int i = 0;
+	while (END == 0)
+	{
+		SS[i] = N % p;
+		N = N / p;
+		i++;
+		if (N < p)
+		{
+			SS[i] = N;
+			END = i;
+		}
+	}
+	for (int i = 0; i <= END; i++)
+	{
+		cout << SS[i];
+	}
 }
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	cout << "ваыпы";
+	zadanie_1(10, 2);
 }
